d1398d4a6c2d860ebb86e2cea9ea35c8d378a04c
lfrenandes@hotmail.com <Goli@thus12>
2020-10-01 15:10:55 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/luludev/animation_login_screen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.luludev.animation_login_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:animation_login_screen/screens/home/home_screen.dart';
+import 'package:animation_login_screen/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animations',
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
